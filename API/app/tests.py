4601171b17356be0677167fadd20ef8708ce5086
GyanP <gyanig72@gmail.com>
2023-05-18 19:00:04 +0530

    write testcases.

--- API/app/tests.py
+++ API/app/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+import os
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+
+from .models import Candidate
+
+
+class RegisterCandidateTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+
+        cls.full_name = "test1"
+        cls.date_of_birth = "2023-01-11"
+        cls.department_type = "IT"
+        cls.experience = "1"
+        cls.resume = (open(os.getcwd() + "/requirements.txt", "rb"), "image/png")
+
+    def tearDown(self, *args, **kwargs):
+        Candidate.objects.all().delete()
+
+    def test_register_candidate(self):
+        payload = {
+            "full_name": self.full_name,
+            "date_of_birth": self.date_of_birth,
+            "department_type": self.department_type,
+            "experience": self.experience,
+            "resume": self.resume,
+        }
+        response = self.client.post(path=reverse("register_candidate"), data=payload)
+
+        data = response.json()
+        self.assertEqual(data["full_name"], self.full_name)
+        self.assertEqual(data["date_of_birth"], self.date_of_birth)
+        self.assertEqual(data["department_type"], self.department_type)
+
+
+class RegisterCandidateTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+
+        cls.full_name = "test1"
+        cls.date_of_birth = "2023-01-11"
+        cls.department_type = "IT"
+        cls.experience = "1"
+        cls.resume = (open(os.getcwd() + "/requirements.txt", "rb"), "image/png")
+
+    def tearDown(self, *args, **kwargs):
+        Candidate.objects.all().delete()
+
+    def test_register_candidate(self):
+        payload = {
+            "full_name": self.full_name,
+            "date_of_birth": self.date_of_birth,
+            "department_type": self.department_type,
+            "experience": self.experience,
+            "resume": self.resume,
+        }
+        response = self.client.post(path=reverse("register_candidate"), data=payload)
+
+        data = response.json()
+        self.assertEqual(data["full_name"], self.full_name)
+        self.assertEqual(data["date_of_birth"], self.date_of_birth)
+        self.assertEqual(data["department_type"], self.department_type)
+
+
+class CandidatesTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.client.credentials(HTTP_X_ADMIN="1")
+
+    def test_list_of_candidate(self):
+        self.client.credentials(HTTP_X_ADMIN="1")
+        response = self.client.get(path=reverse("candidates_list"))
+        data = response.json()
+        self.assertEqual(data, [])
--- API/app/urls.py
+++ API/app/urls.py
@@ -4,7 +4,7 @@ from .views import (CandidateListAPIView, CandidateResumeDownloadAPIView,
                     CandidateView)
 
 urlpatterns = [
-    path("register/", CandidateView.as_view({"post": "create"})),
-    path("candidates/", CandidateListAPIView.as_view()),
+    path("register/", CandidateView.as_view({"post": "create"}), name="register_candidate"),
+    path("candidates/", CandidateListAPIView.as_view(), name="candidates_list"),
     path("candidates/<int:pk>", CandidateResumeDownloadAPIView.as_view()),
 ]
